76883c9768438639fc1fa4182749aa910ccc1ba6
WesleyUckerman <wesleyuckerman@outlook.com>
2024-11-19 16:25:42 -0300

    Initial commit

---
+++ Assets/Tile/CordinateLabeler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+[ExecuteAlways]
+public class CordinateLabeler : MonoBehaviour
+{
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+
+    void Awake() 
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            updateObjectName();
+        }
+    }
+
+    void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+
+    void updateObjectName ()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+}
